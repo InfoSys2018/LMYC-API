5a3260bc62a74c6e1833f22feeab50fa2a587e90
sksmsdkdl23 <philip_choi96@hotmail.com>
2018-04-13 01:29:48 -0700

    Boat picture on create fixed. Default picture will be displayed

--- LmycWeb/Migrations/20180413075308_BoatsConstraints.Designer.cs
+++ LmycWeb/Migrations/20180413075308_BoatsConstraints.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace LmycWeb.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180413075308_BoatsConstraints")]
+    partial class BoatsConstraints
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
